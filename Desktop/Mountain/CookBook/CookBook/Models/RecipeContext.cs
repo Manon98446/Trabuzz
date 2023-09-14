fc9e3d73f65b345d2cbeaeb5ba942548e9b6d32f
Manon <manon.vdm@hotmail.com>
2023-09-14 11:52:52 +0200

    first commit

---
+++ Desktop/Mountain/CookBook/CookBook/Models/RecipeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CookBook.Models;
+
+public class RecipeContext : DbContext
+{
+    public RecipeContext(DbContextOptions<RecipeContext> options)
+        : base(options)
+    { 
+    }
+
+    public DbSet<Recipe> Recipes { get; set; } = null!;
+}
---
+++ Desktop/Mountain/CookBook/CookBook/Models/Recipes.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookBook.Models
+{
+    public class Recipe
+    {
+        [Key] public long IdR { get; set; }
+        public string? NameR { get; set; }
+        public string? Image { get; set; }
+        public int Time { get; set; }
+        public int NumPers { get; set; }
+        public string? Type { get; set; }
+        public string? Ingredients { get; set; }
+        public string? Instructions { get; set; }
+
+    }
+        /*public class Tags
+        {
+            public long IdT { get; set; }
+            public string NameT { get; set; }
+            public string TypeT { get; set; }
+
+        }
+        public class TagsRecipe
+        {
+            public long IdT { get; set; }
+            public long IdR { get; set; }
+
+        }*/
+}
